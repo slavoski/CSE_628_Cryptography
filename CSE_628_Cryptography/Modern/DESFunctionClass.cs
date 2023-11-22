83038f496bb1598e8c32ea907e5107fed1816917
nonya biznes <slavs12@yahoo.com>
2023-11-22 02:50:04 -0700

    Added a ton more functionality

--- CSE_628_Cryptography/Modern/DESFunctionClass.cs
+++ CSE_628_Cryptography/Modern/DESFunctionClass.cs
@@ -9,38 +9,88 @@ namespace CSE_628_Cryptography.Modern
 	{
 		#region member variables
 
+		private string _bit64 = "";
+		private string _bit64Value = "";
+		private string _calculateText = "";
 		private string _crossResult = "";
 		private string _expansionResult = "";
 		private string _finalCrossResult = "";
+		private bool _firstTime = true;
+		private string _initialPermutationResult = "";
 		private string _key = "";
-
+		private string _keyPermutationResult = "";
+		private string _keyShiftResult = "";
 		private string _keyValue = "";
 		private string _leftSide = "";
-		private string _leftValue = "";
 		private string _permutationResult = "";
-
 		private string _rightSide = "";
-		private string _rightValue = "";
+		private int _round = 1;
+		private string _roundKeyPermutationResult = "";
+		private int _s1;
+		private int _s2;
+		private int _s3;
+		private int _s4;
+		private int _s5;
+		private int _s6;
+		private int _s7;
+		private int _s8;
 		private string _sBoxResult = "";
+		private string _firstBitPosition = "";
+		private string _sboxChanged = "";
+
+		#region tables
 
 		private List<int> ExpansionPermutation = new List<int>
 		{
-			32, 1, 2, 3, 4, 5,
-			4, 5, 6, 7, 8, 9,
-			8, 9, 10, 11, 12, 13,
+			32,  1,  2,  3,  4,  5,
+			 4,  5,  6,  7,  8,  9,
+			 8,  9, 10, 11, 12, 13,
 			12, 13, 14, 15, 16, 17,
 			16, 17, 18, 19, 20, 21,
 			20, 21, 22, 23, 24, 25,
 			24, 25, 26, 27, 28, 29,
-			28, 29, 30, 31, 32, 1,
+			28, 29, 30, 31, 32,  1,
+		};
+
+		private List<int> InitialKeyPermutation = new List<int>
+		{
+			57, 49, 41, 33, 25, 17,  9,  1,
+			58, 50, 42, 34, 26, 18, 10,  2,
+			59, 51, 43, 35, 27, 19, 11,  3,
+			60, 52, 44, 36, 63, 55, 47, 39,
+			31, 23, 15,  7, 62, 54, 46, 38,
+			30, 22, 14,  6, 61, 53, 45, 37,
+			29, 21, 13,  5, 28, 20, 12,  4,
+		};
+
+		private List<int> InitialPermutation = new List<int>
+		{
+			58, 50, 42, 34, 26, 18, 10, 2,
+			60, 52, 44, 36, 28, 20, 12, 4,
+			62, 54, 46, 38, 30, 22, 14, 6,
+			64, 56, 48, 40, 32, 24, 16, 8,
+			57, 49, 41, 33, 25, 17,  9, 1,
+			59, 51, 43, 35, 27, 19, 11, 3,
+			61, 53, 45, 37, 29, 21, 13, 5,
+			63, 55, 47, 39, 31, 23, 15, 7,
 		};
 
 		private List<int> Permutation = new List<int>
 		{
-			16, 7, 20, 21, 29, 12, 28, 17,
-			1, 15, 23, 26, 5, 18, 31, 10,
-			2, 8, 24, 14, 32, 27, 3, 9,
-			19, 13, 30, 6, 22, 11, 4, 25,
+			16,  7, 20, 21, 29, 12, 28, 17,
+			 1, 15, 23, 26,  5, 18, 31, 10,
+			 2,  8, 24, 14, 32, 27,  3,  9,
+			19, 13, 30,  6, 22, 11,  4, 25,
+		};
+
+		private List<int> RoundKeyPermutation = new List<int>
+		{
+			14, 17, 11, 24,  1,  5,  3, 28,
+			15,  6, 21, 10, 23, 19, 12,  4,
+			26,  8, 16,  7, 27, 20, 13,  2,
+			41, 52, 31, 37, 47, 55, 30, 40,
+			51, 45, 33, 48, 44, 49, 39, 56,
+			34, 53, 46, 42, 50, 36, 29, 32,
 		};
 
 		private Dictionary<int, List<int>> S1 = new Dictionary<int, List<int>>
@@ -107,9 +157,39 @@ namespace CSE_628_Cryptography.Modern
 			{3, new List<int>{ 02, 01, 14, 07, 04, 10, 08, 13, 15, 12, 09, 00, 03, 05, 06, 11}},
 		};
 
+		private List<int> shiftLeftRounds = new List<int>()
+		{
+			1,2,9,16
+		};
+
+		#endregion tables
+
 		#endregion member variables
 
 		#region properties
+
+		public string Bit64
+		{
+			get => _bit64;
+			set
+			{
+				_bit64 = value;
+				OnPropertyChanged(nameof(Bit64));
+			}
+		}
+
+		public string Bit64Value
+		{
+			get => _bit64Value;
+			set
+			{
+				_bit64Value = value;
+				_firstTime = true;
+				_round = 1;
+				SetCalculate();
+				OnPropertyChanged(nameof(Bit64Value));
+			}
+		}
 
 		public string CrossResult
 		{
@@ -141,6 +221,16 @@ namespace CSE_628_Cryptography.Modern
 			}
 		}
 
+		public string InitialPermutationResult
+		{
+			get => _initialPermutationResult;
+			set
+			{
+				_initialPermutationResult = value;
+				OnPropertyChanged(nameof(InitialPermutationResult));
+			}
+		}
+
 		public string Key
 		{
 			get => _key;
@@ -148,6 +238,29 @@ namespace CSE_628_Cryptography.Modern
 			{
 				_key = value;
 				OnPropertyChanged(nameof(Key));
+				_firstTime = true;
+				_round = 1;
+				SetCalculate();
+			}
+		}
+
+		public string KeyPermutationResult
+		{
+			get => _keyPermutationResult;
+			set
+			{
+				_keyPermutationResult = value;
+				OnPropertyChanged(nameof(KeyPermutationResult));
+			}
+		}
+
+		public string KeyShiftResult
+		{
+			get => _keyShiftResult;
+			set
+			{
+				_keyShiftResult = value;
+				OnPropertyChanged(nameof(KeyShiftResult));
 			}
 		}
 
@@ -171,16 +284,6 @@ namespace CSE_628_Cryptography.Modern
 			}
 		}
 
-		public string LeftSideValue
-		{
-			get => _leftValue;
-			set
-			{
-				_leftValue = value;
-				OnPropertyChanged(nameof(LeftSideValue));
-			}
-		}
-
 		public string PermutationResult
 		{
 			get => _permutationResult;
@@ -201,13 +304,93 @@ namespace CSE_628_Cryptography.Modern
 			}
 		}
 
-		public string RightValue
+		public string RoundKeyPermutationResult
 		{
-			get => _rightValue;
+			get => _roundKeyPermutationResult;
 			set
 			{
-				_rightValue = value;
-				OnPropertyChanged(nameof(RightValue));
+				_roundKeyPermutationResult = value;
+				OnPropertyChanged(nameof(RoundKeyPermutationResult));
+			}
+		}
+
+		public int S1Result
+		{
+			get => _s1;
+			set
+			{
+				_s1 = value;
+				OnPropertyChanged(nameof(S1Result));
+			}
+		}
+
+		public int S2Result
+		{
+			get => _s2;
+			set
+			{
+				_s2 = value;
+				OnPropertyChanged(nameof(S2Result));
+			}
+		}
+
+		public int S3Result
+		{
+			get => _s3;
+			set
+			{
+				_s3 = value;
+				OnPropertyChanged(nameof(S3Result));
+			}
+		}
+
+		public int S4Result
+		{
+			get => _s4;
+			set
+			{
+				_s4 = value;
+				OnPropertyChanged(nameof(S4Result));
+			}
+		}
+
+		public int S5Result
+		{
+			get => _s5;
+			set
+			{
+				_s5 = value;
+				OnPropertyChanged(nameof(S5Result));
+			}
+		}
+
+		public int S6Result
+		{
+			get => _s6;
+			set
+			{
+				_s6 = value;
+				OnPropertyChanged(nameof(S6Result));
+			}
+		}
+
+		public int S7Result
+		{
+			get => _s7;
+			set
+			{
+				_s7 = value;
+				OnPropertyChanged(nameof(S7Result));
+			}
+		}
+
+		public int S8Result
+		{
+			get => _s8;
+			set
+			{
+				_s8 = value;
+				OnPropertyChanged(nameof(S8Result));
 			}
 		}
 
@@ -221,17 +404,43 @@ namespace CSE_628_Cryptography.Modern
 			}
 		}
 
+		public string Calculate
+		{
+			get => _calculateText;
+			set
+			{
+				_calculateText = value;
+				OnPropertyChanged(nameof(Calculate));
+			}
+		}
+
+		public string FirstBitPosition
+		{
+			get => _firstBitPosition;
+			set
+			{
+				_firstBitPosition = value;
+				OnPropertyChanged(nameof(FirstBitPosition));
+			}
+		}
+
+
+		public string SboxChanged 
+		{ 
+			get => _sboxChanged;
+			set
+			{
+				_sboxChanged = value;
+				OnPropertyChanged(nameof(SboxChanged));
+			}
+		}
+
+
 		#endregion properties
 
 		#region commands
 
-		public Command Add32BitsToLeftSideCommand
-		{
-			get;
-			private set;
-		}
-
-		public Command Add32BitsToRightSideCommand
+		public Command Add64BitsToBits64Command
 		{
 			get;
 			private set;
@@ -248,7 +457,7 @@ namespace CSE_628_Cryptography.Modern
 			get;
 			private set;
 		}
-
+	
 		#endregion commands
 
 		#region Constructor
@@ -256,9 +465,9 @@ namespace CSE_628_Cryptography.Modern
 		public DESFunctionClass()
 		{
 			CalculateCommand = new Command(() => CalculateResults());
-			Add32BitsToLeftSideCommand = new Command(() => LeftSide = AddBitsToString(32, LeftSideValue));
-			Add32BitsToRightSideCommand = new Command(() => RightSide = AddBitsToString(32, RightValue));
+			Add64BitsToBits64Command = new Command(() => Bit64 = AddBitsToString(64, Bit64Value));
 			Add64BitsToKeySideCommand = new Command(() => Key = AddBitsToString(64, KeyValue));
+			SetCalculate();
 		}
 
 		#endregion Constructor
@@ -281,7 +490,7 @@ namespace CSE_628_Cryptography.Modern
 			for (int i = 0; i < 48; i++)
 			{
 				var expansionBit = int.Parse(ExpansionResult[i].ToString());
-				var keyBit = int.Parse(Key[i].ToString());
+				var keyBit = int.Parse(RoundKeyPermutationResult[i].ToString());
 
 				CrossResult += (expansionBit ^ keyBit).ToString();
 			}
@@ -306,13 +515,64 @@ namespace CSE_628_Cryptography.Modern
 			}
 		}
 
+		public void CalculateInitialPermuation()
+		{
+			InitialPermutationResult = "";
+
+			foreach (var bit in InitialPermutation)
+			{
+				InitialPermutationResult += Bit64[bit - 1];
+			}
+
+			LeftSide = InitialPermutationResult.Substring(0, 32);
+			RightSide = InitialPermutationResult.Substring(32, 32);
+		}
+
 		public void CalculateResults()
 		{
-			CalculateExpansion();
-			CalculateCrossWithKey();
-			CalculateSBox();
-			CalculateFinalPermuation();
-			CalculateCross();
+			if (Bit64.Length > 0 && Key.Length > 0)
+			{
+				if (_firstTime)
+				{
+					CalculateInitialPermuation();
+					CalculateInitialKeyPermutation();
+					_firstTime = false;
+				}
+				else
+				{
+					LeftSide = RightSide;
+					RightSide = FinalCrossResult;
+				}
+
+				CalculateExpansion();
+				CalculateKeyShift();
+
+				CalculateCrossWithKey();
+				CalculateSBox();
+				CalculateFinalPermuation();
+				CalculateCross();
+				_round++;
+				SetCalculate();
+				SetFirstBitAndSBox();
+			}
+		}
+
+		private void SetFirstBitAndSBox()
+		{
+			var value = RoundKeyPermutationResult.IndexOf('1') + 1;
+			FirstBitPosition = value.ToString();
+
+			var result = (value / 6);
+			if (value % 6 == 0) 
+			{
+				SboxChanged = result.ToString();
+			}
+			else
+			{
+				SboxChanged = (result + 1).ToString();
+			}
+			
+
 		}
 
 		public void CalculateSBox()
@@ -322,19 +582,28 @@ namespace CSE_628_Cryptography.Modern
 			var crossResultsSplit = new List<Tuple<string, string>>();
 			for (int i = 0; i < 8; i++)
 			{
-				var bits = CrossResult.Substring(i, i + 6);
+				var bits = CrossResult.Substring(i * 6, 6);
 
 				crossResultsSplit.Add(new Tuple<string, string>(bits[0].ToString() + bits[5].ToString(), bits.Substring(1, 4)));
 			}
 
-			SBoxResult += ConvertToBit(S1[GetRow(crossResultsSplit[0].Item1)][GetColumn(crossResultsSplit[0].Item2)]);
-			SBoxResult += ConvertToBit(S2[GetRow(crossResultsSplit[1].Item1)][GetColumn(crossResultsSplit[1].Item2)]);
-			SBoxResult += ConvertToBit(S3[GetRow(crossResultsSplit[2].Item1)][GetColumn(crossResultsSplit[2].Item2)]);
-			SBoxResult += ConvertToBit(S4[GetRow(crossResultsSplit[3].Item1)][GetColumn(crossResultsSplit[3].Item2)]);
-			SBoxResult += ConvertToBit(S5[GetRow(crossResultsSplit[4].Item1)][GetColumn(crossResultsSplit[4].Item2)]);
-			SBoxResult += ConvertToBit(S6[GetRow(crossResultsSplit[5].Item1)][GetColumn(crossResultsSplit[5].Item2)]);
-			SBoxResult += ConvertToBit(S7[GetRow(crossResultsSplit[6].Item1)][GetColumn(crossResultsSplit[6].Item2)]);
-			SBoxResult += ConvertToBit(S8[GetRow(crossResultsSplit[7].Item1)][GetColumn(crossResultsSplit[7].Item2)]);
+			S1Result = S1[GetRow(crossResultsSplit[0].Item1)][GetColumn(crossResultsSplit[0].Item2)];
+			S2Result = S2[GetRow(crossResultsSplit[1].Item1)][GetColumn(crossResultsSplit[1].Item2)];
+			S3Result = S3[GetRow(crossResultsSplit[2].Item1)][GetColumn(crossResultsSplit[2].Item2)];
+			S4Result = S4[GetRow(crossResultsSplit[3].Item1)][GetColumn(crossResultsSplit[3].Item2)];
+			S5Result = S5[GetRow(crossResultsSplit[4].Item1)][GetColumn(crossResultsSplit[4].Item2)];
+			S6Result = S6[GetRow(crossResultsSplit[5].Item1)][GetColumn(crossResultsSplit[5].Item2)];
+			S7Result = S7[GetRow(crossResultsSplit[6].Item1)][GetColumn(crossResultsSplit[6].Item2)];
+			S8Result = S8[GetRow(crossResultsSplit[7].Item1)][GetColumn(crossResultsSplit[7].Item2)];
+
+			SBoxResult += ConvertToBit(S1Result);
+			SBoxResult += ConvertToBit(S2Result);
+			SBoxResult += ConvertToBit(S3Result);
+			SBoxResult += ConvertToBit(S4Result);
+			SBoxResult += ConvertToBit(S5Result);
+			SBoxResult += ConvertToBit(S6Result);
+			SBoxResult += ConvertToBit(S7Result);
+			SBoxResult += ConvertToBit(S8Result);
 		}
 
 		public int GetColumn(string column)
@@ -414,19 +683,19 @@ namespace CSE_628_Cryptography.Modern
 			int result = 0;
 			switch (row)
 			{
-				case "0000":
+				case "00":
 					result = 0;
 					break;
 
-				case "0001":
+				case "01":
 					result = 1;
 					break;
 
-				case "0010":
+				case "10":
 					result = 2;
 					break;
 
-				case "0011":
+				case "11":
 					result = 3;
 					break;
 			}
@@ -443,10 +712,59 @@ namespace CSE_628_Cryptography.Modern
 			return results;
 		}
 
+		private void CalculateInitialKeyPermutation()
+		{
+			KeyPermutationResult = "";
+
+			foreach (var bit in InitialKeyPermutation)
+			{
+				KeyPermutationResult += Key[bit - 1];
+			}
+
+			KeyShiftResult = KeyPermutationResult;
+		}
+
+		private void CalculateKeyShift()
+		{
+			ShiftKeyLeft();
+			if (!shiftLeftRounds.Contains(_round))
+			{
+				ShiftKeyLeft();
+			}
+
+			CalculateRoundKeyPermutation();
+		}
+
+		private void CalculateRoundKeyPermutation()
+		{
+			RoundKeyPermutationResult = "";
+
+			foreach (var bit in RoundKeyPermutation)
+			{
+				RoundKeyPermutationResult += KeyShiftResult[bit - 1];
+			}
+		}
+
 		private string ConvertToBit(int value)
 		{
 			var bits = Convert.ToString(value, 2).PadLeft(4, '0');
 			return bits;
+		}
+
+		private void SetCalculate()
+		{
+			Calculate = "Calculate Round " + _round.ToString();
+		}
+
+		private void ShiftKeyLeft()
+		{
+			var leftSide = KeyShiftResult.Substring(0, 28);
+			var rightSide = KeyShiftResult.Substring(28, 28);
+
+			KeyShiftResult = leftSide.Substring(1);
+			KeyShiftResult += leftSide[0];
+			KeyShiftResult += rightSide.Substring(1);
+			KeyShiftResult += rightSide[0];
 		}
 
 		#endregion Methods
